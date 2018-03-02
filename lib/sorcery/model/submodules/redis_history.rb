d05bae863a7e88f1f85956b43bf83c8aea98989c
Igor Petrov <metisigor@gmail.com>
2018-03-02 10:26:08 +0200

    add history settings

--- lib/sorcery/model/submodules/redis_history.rb
+++ lib/sorcery/model/submodules/redis_history.rb
@@ -4,15 +4,29 @@ module Sorcery
 
       module RedisHistory
 
-        SECONDS_IN_A_YEAR = 3600*24*365
-        HISTORY_MAX_SIZE = 20
+        def self.included(base)
+          base.extend(ClassMethods)
+          base.send(:include, InstanceMethods)
+          base.sorcery_config.after_config << :define_redis_history_fields
+        end
+
+        module ClassMethods
+
+          protected
+
+          def define_redis_history_fields
+            sorcery_adapter.define_field sorcery_config.history_size, Integer, default: 20
+            sorcery_adapter.define_field sorcery_config.history_ttl, Integer, default: 3600 * 24 * 365 #year
+          end
+
+        end
 
         module InstanceMethods
 
           def history_push(history_state)
             Redis.current.lpush(security_history_key, Marshal.dump(history_state))
-            Redis.current.ltrim(security_history_key, 0, HISTORY_MAX_SIZE-1)
-            Redis.current.expire(security_history_key, SECONDS_IN_A_YEAR)
+            Redis.current.ltrim(security_history_key, 0, self.history_size - 1)
+            Redis.current.expire(security_history_key, self.history_ttl)
           end
 
           def history_get(history_state)
